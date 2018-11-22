64c56946a0448be05ad6c3ce232680a680981902
Unknown <clement.dechenaux@gmail.com>
2018-11-22 17:25:21 +0100

    tp 3 finished

--- SkeletonJoints/MainWindow.xaml.cs
+++ SkeletonJoints/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Kinect;
 using Coding4Fun.Kinect.Wpf;
+using static SkeletonJoints.SkeletalExtensions;
 
 namespace SkeletonJoints {
     /// <summary>
@@ -28,7 +29,7 @@ namespace SkeletonJoints {
             _sensor = KinectSensor.KinectSensors.FirstOrDefault();
             //KinectSensor.KinectSensors.Where(x => x.Status == KinectStatus.Connected).FirstOrDefault(); 
             if (_sensor != null) {
-                _sensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+                // _sensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
                 _sensor.SkeletonFrameReady += KinectSkeletonFrameReady;
                 _sensor.SkeletonStream.Enable();
                 _sensor.Start();
@@ -55,6 +56,13 @@ namespace SkeletonJoints {
                         foreach (JointType joint in Enum.GetValues(typeof(JointType))) {
                             DrawJoint(skeleton.Joints[joint].ScaleTo(640, 480));
                         }
+                        // Calculate height.
+                        double height = skeleton.Height();
+                        // Display height.
+                        double inches = height * 39.37;
+                        double feet = Math.Floor(inches / 12);
+                        inches = inches - feet * 12;
+                        tblHeight.Text = String.Format("Height: {0}feet {1}inches,{2}m", feet, inches, height);
                     }
                 }
             }
